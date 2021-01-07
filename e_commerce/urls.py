1c9c86ff6ea75995dc1e83d95ff6d3c66a0d80ca
thiagodks <thiagoadriano2010@gmail.com>
2021-01-07 01:53:21 -0300

    update about, contact, home page

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -20,6 +20,6 @@ from .views import home_page, about_page, contact_page
 urlpatterns = [
         path('', home_page),
         path('about/', about_page),
-	path('contact/', contact_page),
+	    path('contact/', contact_page),
         path('admin/', admin.site.urls),
 ]
